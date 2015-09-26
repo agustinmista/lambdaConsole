cfbce26b6528a0c91a009f06c793f97b9edbf1b9
Agustín Mista <mista.agustin@gmail.com>
2015-09-26 15:40:07 -0300

    final fixes

--- src/Untyped.hs
+++ src/Untyped.hs
@@ -22,7 +22,7 @@ num n = Abs "f" (Abs "x" (app n))
 -----------------------
 
 totParser :: Parser a -> Parser a
-totParser p = do 
+totParser p = do
                   whiteSpace untyped
                   t <- p
                   eof
@@ -33,7 +33,7 @@ untyped :: TokenParser u
 untyped = makeTokenParser (haskellStyle { identStart = letter <|> char '_',
                                           reservedNames = ["def"],
                                           opLetter = oneOf "" })
- 
+
 -- Parser para comandos
 parseStmt :: Parser a -> Parser (Stmt a)
 parseStmt p = do reserved untyped "def"
@@ -43,14 +43,14 @@ parseStmt p = do reserved untyped "def"
                  return (Def x t)
              <|> fmap Eval p
 
- 
+
 parseTermStmt :: Parser (Stmt Term)
 parseTermStmt = fmap (fmap conversion) (parseStmt parseLamTerm)
 
-{-  
+{-
 
 --  Gramatica del calculo lambda extendido
-    
+
     <atom>   := <var> | <number> | '(' <term> ')'
 
     <ids>    := <var> ( e | <ids> )
@@ -58,13 +58,13 @@ parseTermStmt = fmap (fmap conversion) (parseStmt parseLamTerm)
     <abs>    := '\' <ids> '.' <term>
 
     <notAbs> := <atom> | <notAbs> <notApp>
-    
+
     <notApp> := <atom> | <abs>
-    
+
     <term>   := <abs> | <notAbs>
 
  -- Gramatica del calculo lambda extendido sin recursion a izq
-    
+
     <atom>   := <var> | <number> | '(' <term> ')'
 
     <ids>    := <var> ( e | <ids> )
@@ -73,11 +73,11 @@ parseTermStmt = fmap (fmap conversion) (parseStmt parseLamTerm)
 
     <notAbs> := <atom> <notAbs'>
     <notAbs'> := <notApp> <notAbs'> | e
-    
+
     <notApp> := <atom> | <abs>
-    
+
     <term>   := <abs> | <notAbs>
-    
+
 -}
 
 
@@ -105,28 +105,28 @@ parseNotAbs :: Parser LamTerm
 parseNotAbs = do atom <- parseAtom
                  f_nabs' <- parseNotAbs'
                  return $ f_nabs' atom
-                 
+
 parseNotAbs' :: Parser (LamTerm -> LamTerm)
 parseNotAbs' = do napp <- parseNotApp
                   f_nabs' <- parseNotAbs'
                   return $ \x -> f_nabs' (App x napp)
            <|> return id
-                  
+
 parseNotApp :: Parser LamTerm
 parseNotApp = parseAtom <|> parseAbs
 
 
--- Parser para LamTerms 
+-- Parser para LamTerms
 parseLamTerm :: Parser LamTerm
 parseLamTerm = parseAbs <|> parseNotAbs
-                
-                
+
+
 -- conversion a términos localmente sin nombres
 conversion  :: LamTerm -> Term
 conversion  = toTerm []
 
 toTerm :: [String] -> LamTerm ->  Term
-toTerm names (Abs name t) = Lam $ toTerm (name:names) t 
+toTerm names (Abs name t) = Lam $ toTerm (name:names) t
 toTerm names (App t1 t2) = (toTerm names t1) :@: (toTerm names t2)
 toTerm names (LVar name) = case elemIndex name names
                            of Just index -> Bound index
@@ -145,7 +145,7 @@ vapp :: Value -> Value -> Value
 vapp (VNeutral t1) t2 = VNeutral $ NApp t1 t2
 vapp (VLam f)      t2 = f t2
 
- 
+
 eval :: [(Name, Value)] -> Term -> Value
 eval  e t = eval' t (e,[])
 
@@ -155,11 +155,10 @@ eval' (Bound ii)  (env, bv) = bv !! ii
 eval' (Lam e)     (env, bv) = VLam (\x -> eval' e (env, (x:bv)))
 eval' (Free n)    (env, bv) = lookFor n env
                                 where lookFor n [] = VNeutral $ NFree $ n
-                                      lookFor n ((k, v) : xs) = 
+                                      lookFor n ((k, v) : xs) =
                                             if k == n
                                             then v
                                             else lookFor n xs
-
 
 -------------------------------
 -- Sección 4
@@ -173,4 +172,3 @@ quote' (VNeutral (NFree (Global n))) i = Free $ Global n
 quote' (VNeutral (NFree (Quote  n))) i = Bound (i - n - 1)
 quote' (VNeutral (NApp  n v))        i = (quote' (VNeutral n) i) :@: (quote' v i)
 quote' (VLam f)                      i = Lam $ quote' (f (VNeutral $ NFree $ Quote i)) (i+1)
-
